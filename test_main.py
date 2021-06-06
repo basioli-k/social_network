26695322c31f82209be2f75acbc6b548ce3a9d10
basioli-k <k.basioli@gmail.com>
2021-06-06 20:01:41 +0200

    testing query time

--- test_main.py
+++ test_main.py
@@ -1,30 +1,26 @@
 import sys
+import random
+import time
 
 sys.path.append('./entities')
 sys.path.append('./database')
+sys.path.append('./data_prep')
 
 from database import *
 from person import *
+from college import *
+from db_create import *
 
 if __name__ == "__main__":
-    person = Person("K", "b", id=79)
 
-    # first_rec = [data[0] for data in person.get_business_recommendation("./database/database.cfg")]
-    
-    # second_rec = [data[0] for data in person.get_business_recommendation_2("./database/database.cfg")]
+    for i in range(50, 1050, 50):
+        POPULATION = i
+        generate_data("./data_prep/data.cfg", "./data_prep/names-by-gender.csv", POPULATION)
+        fill_database("./database/database.cfg")
+        for i in range(10): # za 10 razlicitih ljudi ovo ucini
+            person = Person("K", "b", id=random.randint(1, POPULATION))
+            t = person.get_personal_recommendation("./database/database.cfg")
+            print_to_file("./analysis/personal.csv", "population,time",f"{POPULATION},{t}")
 
-    # for person in person.get_personal_recommendation_2("./database/database.cfg"):
-    #     print(str(person))
-
-    print(Person.get_max_id("./database/database.cfg"))
-
-    #print(len(set(person.get_personal_recommendation("./database/database.cfg")) & set(person.get_personal_recommendation_2("./database/database.cfg") ) ))
-
-    # c = 0
-    # for el1 in first_rec:
-    #     for el2 in second_rec:
-    #         if el1.id == el2.id:
-    #             c += 1
-
-    # print(len(set(first_rec) & set(second_rec)))
-    
+            t = person.get_business_recommendation("./database/database.cfg") 
+            print_to_file("./analysis/business.csv", "population,time",f"{POPULATION},{t}")
